cd7a65d866d60c0fa085bbf09f0ffc3831f1dbfe
Adriana <129066063+adrianaarang@users.noreply.github.com>
2026-03-26 15:47:08 +0100

    Refactor patient handling with methods for priority and destination

--- hospital.py
+++ hospital.py
@@ -1,44 +1,23 @@
 from paciente import Paciente
 
 
+pacientes = [
 
-paciente1 = Paciente(70, 90, 38.5, True, False, False)
-paciente2 = Paciente(8, 110, 39.2, False, True, False)
-paciente3 = Paciente(30, 80, 36.5, False, True, False)
-paciente4 = Paciente(0, 80, 36.5, False, True, False)
+    Paciente(70, 90, 38.5, True, False, False),
+    Paciente(8, 110, 39.2, False, True, False),
+    Paciente(30, 80, 36.5, False, True, False),
+    Paciente(0, 80, 36.5, False, True, False)
 
-
-
-pacientes = [paciente1, paciente2, paciente3, paciente4]
-
+]
 
 
 for paciente in pacientes:
 
-
-    if paciente.es_emergencia_critica or paciente.temperatura > 41:
-        prioridad = "Roja"
-
-    elif paciente.temperatura >= 39 and paciente.frecuencia_cardiaca > 100:
-        prioridad = "Naranja"
-
-    elif paciente.edad < 12 or paciente.edad > 65:
-        prioridad = "Amarilla"
-
-    else:
-        prioridad = "Verde"
-
-
-
-    if paciente.tiene_sintomas_infecciosos and not paciente.esta_vacunado:
-        destino = "Box de Aislamiento"
-
-    else:
-        destino = "Sala de Espera General"
-
-
+    prioridad = paciente.clasificar_prioridad()
+    destino = paciente.asignar_destino()
 
     print("Resumen del paciente")
     print("Edad:", paciente.edad)
     print("Prioridad:", prioridad)
     print("Destino:", destino)
+    print("---------------------")
