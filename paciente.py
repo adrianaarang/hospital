c861d3ea9c27993d29508c275bfb854bad59328a
Adriana <129066063+adrianaarang@users.noreply.github.com>
2026-03-26 15:46:29 +0100

    Add priority classification and destination assignment methods

--- paciente.py
+++ paciente.py
@@ -9,3 +9,27 @@ class Paciente:
         self.tiene_sintomas_infecciosos = tiene_sintomas_infecciosos
         self.esta_vacunado = esta_vacunado
         self.es_emergencia_critica = es_emergencia_critica
+
+
+    def clasificar_prioridad(self):
+
+        if self.es_emergencia_critica or self.temperatura > 120:
+            return "Roja"
+
+        elif self.temperatura >= 39 and self.frecuencia_cardiaca > 100:
+            return "Naranja"
+
+        elif self.edad < 12 or self.edad > 65:
+            return "Amarilla"
+
+        else:
+            return "Verde"
+
+
+    def asignar_destino(self):
+
+        if self.tiene_sintomas_infecciosos and not self.esta_vacunado:
+            return "Box de Aislamiento"
+
+        else:
+            return "Sala de Espera General"
